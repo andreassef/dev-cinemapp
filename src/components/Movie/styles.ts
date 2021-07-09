c8bd2696d2df642087d81e94f7c286adb3548589
andreassef <andreassef02@gmail.com>
2021-07-08 20:39:10 -0400

    [NEW] - Folha de estilo do card de Movie

---
+++ src/components/Movie/styles.ts
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 20,
+    borderRadius: 8,
+    height: 70,
+    borderWidth:1,
+    borderColor: theme.colors.heading,
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+  },
+  content: {
+    flex: 1,
+  },
+  title: {
+    fontFamily: theme.fonts.title700,
+    color: theme.colors.heading,
+    fontSize: 18,
+  },
+  year: {
+    fontFamily: theme.fonts.title500,
+    color: theme.colors.heading,
+    fontSize: 15,
+    lineHeight: 21
+  }
+});
