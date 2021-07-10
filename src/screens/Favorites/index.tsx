f4a0daa7d4d00f7064a7c0fbccd58f4cfb4ef491
andreassef <andreassef02@gmail.com>
2021-07-10 16:31:16 -0400

    [UPDATE] - Listando filmes favoritos

--- src/screens/Favorites/index.tsx
+++ src/screens/Favorites/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {
   View,
-  Text
+  Text,
+  FlatList
 } from 'react-native';
-
+import { Movie } from '../../components/Movie';
+import { useMovies } from '../../hooks/listMovies';
+import { MovieProps } from '../Movies';
 import { styles } from './styles';
 
+import { loadFavoritesMovies } from '../../storage';
+
 export function Favorites(){
+  const [movies, setMovies] = useState<MovieProps[]>([])
+  // const {movies} = useMovies();
+
+  useEffect(() => {
+    async function loadMovies() {
+      const moviesStored = await loadFavoritesMovies();
+      setMovies(moviesStored);
+    }
+
+    loadMovies();
+  }, [movies]);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -15,6 +32,23 @@ export function Favorites(){
           Aqui estão seus filmes favoritos
         </Text>
       </View>
+
+      <FlatList
+          data={movies}
+          keyExtractor={ item => item.imdbID }
+          renderItem={({item}) => (
+            <Movie 
+              Title={item.Title}
+              Year={item.Year}
+              imdbID={item.imdbID}
+              fav={true}
+              activeOpacity={0.7}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          style={{flex: 1}}
+        >
+      </FlatList>
     </View>
   );
 }
